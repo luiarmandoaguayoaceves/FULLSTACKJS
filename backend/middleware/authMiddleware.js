14a28f473b9a6a2b3bacfa30001f8c0b3337929c
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2024-02-14 15:58:51 -0600

    FEAT: Autenticar y validar el token

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,27 @@
-const checkAuth = (req, res, next) => {
-    console.log('Desde el Middleware');
+import jwt from 'jsonwebtoken'
+import Veterinario from '../models/Veterinario.js';
+
+const checkAuth = async (req, res, next) => {
+    let token;
+    if(
+        req.headers.authorization && 
+        req.headers.authorization.startsWith('Bearer')
+    ){
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            const decode = jwt.verify(token,process.env.JWT_SECRET);
+            req.veterinario = await Veterinario.findById(decode.id).select("-password -token -confirmado");
+            return next()
+        } catch (error) {
+            const e = new Error('Token no valido');
+            res.status(403).json({msg: e.message});
+        }
+    }
+
+    if(token){
+        const error = new Error('Token no valido o inexistente');
+        return res.status(403).json({msg: error.message});
+    }
     next()
 }
 
