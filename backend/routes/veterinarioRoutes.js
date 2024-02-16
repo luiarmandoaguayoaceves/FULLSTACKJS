463fda53897dfdc00fa664a70333b8424f2f06b4
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2024-02-15 18:39:15 -0600

    FEAT: Cambio o perdida de contraseña. Area Veterinario

--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,14 +1,26 @@
 import express from "express";
-import { perfil, registrar, confirmar, autenticar } from "../controllers/veterinarioController.js";
+import { 
+    perfil, 
+    registrar, 
+    confirmar, 
+    autenticar, 
+    comprobarToken, 
+    nuevoPassword, 
+    olvidePassword 
+} from "../controllers/veterinarioController.js";
 import checkAuth from '../middleware/authMiddleware.js'
 
 
 const router = express.Router();
-
+//Area publica
 router.post('/', registrar);
 router.get('/confirmar/:token', confirmar);
 router.post('/login', autenticar);
+router.post('/olvide-password', olvidePassword);
+router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword)
 
-router.get('/perfil',checkAuth, perfil);
+
+//Area privada
+router.get('/perfil', checkAuth, perfil);
 
 export default router;
